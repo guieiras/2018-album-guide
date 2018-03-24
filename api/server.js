e6e7c577acf3b123be0a418f82042e65932426cc
Guilherme Eiras <guialbertoeiras@hotmail.com>
2018-03-24 15:21:06 -0300

    Added Body Parser and CORS

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
+
 const mongoose = require('mongoose');
 const Album = require('./models').Album;
 
@@ -10,6 +13,10 @@ require('dotenv').config();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URL);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 require('./routes')(app);
 
